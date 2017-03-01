4017392b0a381351b77cec8d75384684279435c7
Lee Paludan <Leepaludan@gmail.com>
2017-03-01 12:35:10 +0100

    Added: SQLite, DB Migration/Update, merged everything DONE

---
+++ GirafWebApi/GirafWebApi/Contexts/GirafDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GirafWebApi.Models;
+using Microsoft.Data.Sqlite;
+
+namespace GirafWebApi.Contexts
+{
+    public class GirafDbContext : IdentityDbContext<GirafUser>
+    {
+        public GirafDbContext(DbContextOptions<GirafDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "Caskit.db" };
+        	var connectionString = connectionStringBuilder.ToString();
+        	var connection = new SqliteConnection(connectionString);
+
+			optionsBuilder.UseSqlite(connection);
+		}
+    }
+}
---
+++ GirafWebApi/GirafWebApi/Controllers/PublicPictogramController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GirafWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PublicPictogramController : Controller
+    {
+         
+    }
+}
