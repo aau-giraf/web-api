27701102f602e7532b52e4e139edacc2a293f2ca
Tobias Morell <tmorel14@student.aau.dk>
2017-04-18 11:04:05 +0200

    UnitTest : PictogramController

--- GirafRest.Test/Controllers/PictogramControllerTest.cs
+++ GirafRest.Test/Controllers/PictogramControllerTest.cs
@@ -2,53 +2,68 @@ using System;
 using System.Linq;
 using Xunit;
 using Moq;
-using System.Collections.Generic;
 using GirafRest.Models;
 using GirafRest.Controllers;
 using GirafRest.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GirafRest.Test
 {
     public class PictogramControllerTest
     {
         private readonly PictogramController pictogramController;
+        private readonly Mock<GirafDbContext> dbMock;
+        private readonly Mock<UserManager<GirafUser>> umMock;
+        private readonly Mock<ILoggerFactory> lfMock;
+        private readonly List<string> logs;
+
 
         public PictogramControllerTest()
         {
-            var mockContext = new Mock<GirafDbContext> ();
-            mockContext.Setup(c => addTestSessions(c));
-            
-            var mockUserManager = new Mock<UserManager<GirafUser>>();
-            mockUserManager.Setup(um => addUsers(um));
+            dbMock = new Mock<GirafDbContext> ();
+            dbMock.Setup(c => c.Pictograms.AddRange(testSessions()));
+            dbMock.Setup(c => c.SaveChanges());
 
-            var mockHostingEnvironment = new Mock<IHostingEnvironment>();
+            umMock = new Mock<UserManager<GirafUser>>();
+            umMock.Setup(um => addUsers(um));
 
-            var mockLogger = new Mock<ILoggerFactory>();
+            logs = new List<string>();
+            var lMock = new Mock<ILogger>();
+            lMock.Setup(x => x.LogInformation(It.IsAny<string>())).Callback((string s) => logs.Add(s));
+            lMock.Setup(x => x.LogError(It.IsAny<string>())).Callback((string s) => logs.Add(s));
 
-            pictogramController = new PictogramController(mockContext.Object, mockUserManager.Object, mockHostingEnvironment.Object, mockLogger.Object);
+            lfMock = new Mock<ILoggerFactory>();
+            lfMock.Setup(lf => lf.CreateLogger(It.IsAny<string>())).Returns(lMock.Object);
+
+            pictogramController = new PictogramController(dbMock.Object, umMock.Object, lfMock.Object);
         }
 
         [Fact]
-        public void Get_NoLogin_ExpectPublicPictograms()
+        public void AccessExistingPublic_Expect200OK()
         {
-            //var result = pictogramController.ReadPictograms();
+            Pictogram p = testSessions().Where(pict => pict.AccessLevel == AccessLevel.PUBLIC).First();
+            var res = pictogramController.ReadPictogram((int) p.Id);
+            IActionResult aRes = res.Result;
+
+            Assert.IsType<JsonResult>(aRes);
+            var jRes = aRes as JsonResult;
+            Assert.True(jRes.StatusCode == 400);
         }
 
-        public virtual void addTestSessions(GirafDbContext context) {
-            var sessions = new List<PictoFrame> {
+        public List<Pictogram> testSessions() {
+            var sessions = new List<Pictogram> {
                 new Pictogram("Public Picto1", AccessLevel.PUBLIC),
                 new Pictogram("Public Picto2", AccessLevel.PUBLIC),
                 new Pictogram("No restrictions", AccessLevel.PUBLIC),
                 new Pictogram("Restricted", AccessLevel.PRIVATE),
                 new Pictogram("Private Pictogram", AccessLevel.PRIVATE)
             };
-            foreach(var p in sessions) {
-                context.Add(p);
-            }
-            context.SaveChanges();
+            
+            return sessions;
         }
         public virtual void addUsers(UserManager<GirafUser> userManager) {
             var users = new List<GirafUser> {
