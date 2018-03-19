83ae002f1cdc25bd8dffff7a1c9113a5fa72cfdb
Mathias Ibsen <mathias@ibsen.name>
2018-03-19 22:22:24 +0100

    Just don't

--- GirafIntegrationTest/testLib.py
+++ GirafIntegrationTest/testLib.py
@@ -18,14 +18,3 @@ def newTest():
 
 def isLoggedIn():
 	return request('GET', 'user/username')['success']
-
-if(datetime.datetime.today().weekday() > 4):
-	# Send en mail til anton om at stræberne kører integrationtests i weekenderne:
-	message = getpass.getuser() + ' is a nerd and runs integration-tests on the weekends!'
-	comm = ['curl', '-s', 'https://antonchristensen.net', '-c' ,'cookie']
-	process = subprocess.Popen(comm, stdout=subprocess.PIPE)
-	output, error = process.communicate()
-	tokenIndex = output.find(br'<input name="anti_pihilili_token" type="hidden" value="')
-	comm = ['curl', '-sX', 'POST', 'https://antonchristensen.net', '-b', 'cookie','-H', 'Cache-Control: no-cache','-H', 'content-type: multipart/form-data', '-F', 'name=spam', '-F', 'anti_pihilili_token=' + output[tokenIndex+55:tokenIndex+55+10].decode("utf-8"), '-F', 'email=spam', '-F', 'message='+message]
-	process = subprocess.Popen(comm, stdout=subprocess.PIPE)
-	print("lolnerd")
