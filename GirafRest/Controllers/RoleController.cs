05aeca4f8f18d40e8b8f5228fdaf0eb4449789c8
Unknown <asgerhb@gmail.com>
2018-03-21 08:01:54 +0100

    More nullchecks to keep from crashing

--- GirafRest/Controllers/RoleController.cs
+++ GirafRest/Controllers/RoleController.cs
@@ -45,6 +45,10 @@ namespace GirafRest.Controllers
         public async Task<Response<string>> GetGirafRole()
         {
             var user = await _giraf.LoadUserAsync(HttpContext.User);
+            
+            if (user == null)
+                return new Response<string>("");
+            
             var role = await _giraf._userManager.GetRolesAsync(user);
 
             if (role.Count == 0)
