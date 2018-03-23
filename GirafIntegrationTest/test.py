9a0897e92951f4825e5bc3c84c361ec432f9eb8c
Asger Horn <asgerhb@gmail.com>
2018-03-23 14:25:09 +0100

    Imoprove homebrew test framework

--- GirafIntegrationTest/test.py
+++ GirafIntegrationTest/test.py
@@ -2,64 +2,64 @@ from testLib import *
 import time
 import json
 
-newTest() 
-# Login with valid credentials returns with "success"=True and "data"
-response = request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')
-assert(response['success'] is True)
-assert('data' in response)
+acc = controllerTest("Account Controller")
+
+acc.newTest('Login with valid credentials returns with "success"=True and "data"') 
+response = acc.request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')
+acc.ensure(response['success'] is True)
+acc.ensure('data' in response)
 auth = response['data']
 
-response = request('GET', 'user/username', auth=auth)
-assert(response['success'] is True)
-assert(response['data'] == "Graatand")
+response = acc.request('GET', 'user/username', auth=auth)
+acc.ensure(response['success'] is True)
+acc.ensure(response['data'] == "Graatand")
 
-response = request('GET', 'user/username') # No authorization header
-assert(response['success'] is False)
-assert('data' not in response or response['data'] == None)
+response = acc.request('GET', 'user/username') # No authorization header
+acc.ensure(response['success'] is False)
+acc.ensure('data' not in response or response['data'] == None)
 
-newTest() 
-# Login with invalid password returns with "success"=False and no "data"
-response = request('POST', 'account/login', '{"username": "Graatand", "password": "wrongPassword"}')
-assert(response['success'] is False)
-assert('data' not in response or response['data'] == None)
+acc.newTest('Login with invalid password returns with "success"=False and no "data"') 
+response = acc.request('POST', 'account/login', '{"username": "Graatand", "password": "wrongPassword"}')
+acc.ensure(response['success'] is False)
+acc.ensure('data' not in response or response['data'] == None)
 
-newTest()
-# Login with invalid username returns with "success"=False and no "data"
-response = request('POST', 'account/login', '{"username": "WrongGraatand", "password": "password"}')
-assert(response['success'] is False)
-assert('data' not in response or response['data'] == None)
+acc.newTest('Login with invalid username returns with "success"=False and no "data"')
+response = acc.request('POST', 'account/login', '{"username": "WrongGraatand", "password": "password"}')
+acc.ensure(response['success'] is False)
+acc.ensure('data' not in response or response['data'] == None)
 
-newTest()
-# Register new user, without logging in
+acc.newTest('Register new user, without logging in')
 # Will generate a unique enough number, so the user isn't already created
 gunnarUsername = str(time.time())
-response = request('POST', 'account/register', '{"username": "' + gunnarUsername + '","password": "password","departmentId": 1}')
-assert(response['success'] is True)
+response = acc.request('POST', 'account/register', '{"username": "' + gunnarUsername + '","password": "password","departmentId": 1}')
+acc.ensure(response['success'] is True)
+
 # Login as new user
-response = request('POST', 'account/login', '{"username": "' + gunnarUsername + '", "password": "password"}')
-assert(response['success'] is True)
+response = acc.request('POST', 'account/login', '{"username": "' + gunnarUsername + '", "password": "password"}')
+acc.ensure(response['success'] is True)
 gunnarToken = response['data']
+
 # Check if token is valid
-response = request('GET', 'user/username', auth=gunnarToken)
-assert(response['success'] is True)
-assert(response['data'] == gunnarUsername)
+response = acc.request('GET', 'user/username', auth=gunnarToken)
+acc.ensure(response['success'] is True)
+acc.ensure(response['data'] == gunnarUsername)
 
 # Check that gunnar is a citizen
-response = request('GET', 'user', auth=gunnarToken)
-assert(response['success'] is True)
-assert(response['data']['roleName'] == 'Citizen')
+response = acc.request('GET', 'user', auth=gunnarToken)
+acc.ensure(response['success'] is True)
+acc.ensure(response['data']['roleName'] == 'Citizen')
 
 # Login as department
-response = request('POST', 'account/login', '{"username": "Tobias", "password": "password"}')
-assert(response['success'] is True)
+response = acc.request('POST', 'account/login', '{"username": "Tobias", "password": "password"}')
+acc.ensure(response['success'] is True)
 tobiasToken = response['data']
 
 # Add gunnar to guardians
-response = request('POST', 'role/guardian/' + gunnarUsername, auth=tobiasToken)
-assert(response['success'] is True)
+response = acc.request('POST', 'role/guardian/' + gunnarUsername, auth=tobiasToken)
+acc.ensure(response['success'] is True)
 
 # Check that gunnar is a guardian
-response = request('GET', 'user', auth=gunnarToken)
+response = acc.request('GET', 'user', auth=gunnarToken)
 print(response)
-assert(response['success'] is True)
-assert(response['data']['roleName'] == 'Guardian')
+acc.ensure(response['success'] is True)
+acc.ensure(response['data']['roleName'] == 'Guardian')
--- GirafIntegrationTest/testLib.py
+++ GirafIntegrationTest/testLib.py
@@ -3,18 +3,33 @@ import json
 import datetime
 import getpass
 
-def call(*comm):
-	process = subprocess.Popen(comm, stdout=subprocess.PIPE)
-	output, error = process.communicate()
-	return json.loads(output)
-def login():
-	assert(request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')['success'])
+class controllerTest:
+	name = "unkonwn controller"
+	currentTest = "unknown test"
+	
+	def __init__(self, name):
+		self.name = name
+	
+	def call(self, *comm):
+		process = subprocess.Popen(comm, stdout=subprocess.PIPE)
+		output, error = process.communicate()
+		return json.loads(output)
 
-def request(requestType, url, data='', auth='nokey'):
-	return call('curl',  '-sX', requestType, 'http://localhost:5000/v1/'+url,'-H', 'accept: text/plain', '-H', "Authorization: bearer " + auth,'-H', 'Content-Type: application/json-patch+json' ,'-d',data)
+	def login(self):
+		assert(self.request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')['success'])
 
-def newTest():
-	assert(request('POST', 'account/logout')['success'])
+	def request(self, requestType, url, data='', auth='nokey'):
+		return self.call('curl',  '-sX', requestType, 'http://localhost:5000/v1/'+url,'-H', 'accept: text/plain', '-H', "Authorization: bearer " + auth,'-H', 'Content-Type: application/json-patch+json' ,'-d',data)
 
-def isValidAuth(auth):
-	return request('GET', 'user/username', auth=auth)['success']
+	def newTest(self, title):
+		currentTest = title
+		self.ensure(self.request('POST', 'account/logout')['success'])
+
+	def isValidAuth(self, auth):
+		return self.request('GET', 'user/username', auth=auth)['success']
+		
+	def ensure(self, fact, errormessage=""):
+		if fact:
+			return;
+		else:
+			print("ENSURE failed in " + name + " for test `" + currentTest + "`\n " + errorMessage + "\n======================\n\n")
