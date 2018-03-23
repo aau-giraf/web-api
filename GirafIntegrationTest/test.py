ab9351999197366eae65bfb92916e248dc79d1c4
Mathias Pihl <mathias@pihlerne.dk>
2018-03-23 12:16:44 +0100

    Adds more integration test

--- GirafIntegrationTest/test.py
+++ GirafIntegrationTest/test.py
@@ -1,39 +1,65 @@
 from testLib import *
+import time
+import json
 
-# Test that newTest works as expected
-response = request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')
-newTest()
-assert(not isLoggedIn())
-
-newTest()
+newTest() 
 # Login with valid credentials returns with "success"=True and "data"
 response = request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')
 assert(response['success'] is True)
 assert('data' in response)
+auth = response['data']
 
-response = request('GET', 'user/username') # Does not work because it needs authentication, which is not implemented yet
+response = request('GET', 'user/username', auth=auth)
 assert(response['success'] is True)
 assert(response['data'] == "Graatand")
 
-newTest()
+response = request('GET', 'user/username') # No authorization header
+assert(response['success'] is False)
+assert('data' not in response or response['data'] == None)
+
+newTest() 
 # Login with invalid password returns with "success"=False and no "data"
 response = request('POST', 'account/login', '{"username": "Graatand", "password": "wrongPassword"}')
 assert(response['success'] is False)
 assert('data' not in response or response['data'] == None)
-assert(not isLoggedIn())
 
 newTest()
 # Login with invalid username returns with "success"=False and no "data"
 response = request('POST', 'account/login', '{"username": "WrongGraatand", "password": "password"}')
 assert(response['success'] is False)
 assert('data' not in response or response['data'] == None)
-assert(not isLoggedIn())
 
 newTest()
-# Register new user
-response = request('POST', 'account/register', '{"username": "Gunnar","password": "password","departmentId": 1}')
+# Register new user, without logging in
+# Will generate a unique enough number, so the user isn't already created
+gunnarUsername = str(time.time())
+response = request('POST', 'account/register', '{"username": "' + gunnarUsername + '","password": "password","departmentId": 1}')
 assert(response['success'] is True)
-response = request('POST', 'account/login', '{"username": "Gunnar", "password": "password"}')
+# Login as new user
+response = request('POST', 'account/login', '{"username": "' + gunnarUsername + '", "password": "password"}')
 assert(response['success'] is True)
-response = request('POST', 'user/username')
-assert(response['data'] == "Gunnar")
+gunnarToken = response['data']
+# Check if token is valid
+response = request('GET', 'user/username', auth=gunnarToken)
+assert(response['success'] is True)
+assert(response['data'] == gunnarUsername)
+
+# Check that gunnar is a citizen
+response = request('GET', 'user', auth=gunnarToken)
+assert(response['success'] is True)
+assert(response['data']['roleName'] == 'Citizen')
+
+# Login as department
+response = request('POST', 'account/login', '{"username": "Tobias", "password": "password"}')
+assert(response['success'] is True)
+tobiasToken = response['data']
+
+# Add gunnar to guardians
+response = request('POST', 'role/guardian/' + gunnarUsername, auth=tobiasToken)
+assert(response['success'] is True)
+
+# Check that gunnar is a guardian
+response = request('GET', 'user', auth=gunnarToken)
+print(response)
+assert(response['success'] is True)
+assert(response['data']['roleName'] == 'Guardian')
--- GirafIntegrationTest/testLib.py
+++ GirafIntegrationTest/testLib.py
@@ -6,15 +6,15 @@ import getpass
 def call(*comm):
 	process = subprocess.Popen(comm, stdout=subprocess.PIPE)
 	output, error = process.communicate()
-	return json.loads(output) 
+	return json.loads(output)
 def login():
 	assert(request('POST', 'account/login', '{"username": "Graatand", "password": "password"}')['success'])
 
-def request(requestType, url, data=''):
-	return call('curl',  '-sX', requestType, 'http://localhost:5000/v1/'+url,'-H', 'accept: text/plain','-H', 'Content-Type: application/json-patch+json' ,'-d',data)
+def request(requestType, url, data='', auth='nokey'):
+	return call('curl',  '-sX', requestType, 'http://localhost:5000/v1/'+url,'-H', 'accept: text/plain', '-H', "Authorization: bearer " + auth,'-H', 'Content-Type: application/json-patch+json' ,'-d',data)
 
 def newTest():
 	assert(request('POST', 'account/logout')['success'])
 
-def isLoggedIn():
-	return request('GET', 'user/username')['success']
+def isValidAuth(auth):
+	return request('GET', 'user/username', auth=auth)['success']
