7c82aa80db15bd7d327c70d2b28e285784a522cb
AsgerHB <asgerhb@gmail.com>
2018-04-11 15:13:33 +0200

    Restore testLib methods to use new framework

--- GirafIntegrationTest/testLib.py
+++ GirafIntegrationTest/testLib.py
@@ -25,11 +25,6 @@ class Request:
                      '{{"username": "{0}", "password": "{1}"}}'.format(username, password))
         return self.JSON()['data']
 
-    def request(self, requestType, url, data='', auth='nokey'):
-        Request.connection.request(requestType, "/v1/" + url, data,
-                                   {**Request.headers, **{'Authorization': 'bearer {0}'.format(auth)}})
-        self.response = Request.connection.getresponse()
-
     def JSON(self):
         return json.loads(self.response.read())
 
@@ -40,118 +35,56 @@ class Request:
 class Test:
     url = "http://0:5000/v1/"
 
-    runCount = 0  # Accross all Tests
-    failed = False
-    failedCount = 0  # Accross all Tests
 
-    name = "unkonwn controller"
-    current = "unknown test"
+def ensureValidResponse(response, check):
+    return check.is_not_none(response, message='Invalid response. Likely a 404 or a stacktrace.')
 
-    def __init__(self, name):
-        self.name = name
 
-    def login(self, username, password='password'):
-        response = requests.post(Test.url + 'account/login', json={"username": username, "password": password})
-        self.ensure(response.json()['success'], 'Failed to log in as {0} using password {1}'.format(username, password))
-        return response.json()['data']
+def ensureSuccess(response, check):
+    if not ensureValidResponse(response, check):
+        return False
 
-    def new(self, title):
-        if self.failed:
-            Test.failedCount += 1
-        Test.runCount += 1
+    errormessages = ''
 
-        self.current = title
-        self.failed = False
+    for message in response['errorProperties']:
+        errormessages += '\nMessage:  ' + message
 
-    def ensure(self, fact, errormessage="", calldepth=1):
-        if fact:
-            return False
-        else:
-            # Extra thick line dividing different tests.
-            if not self.failed:
-                print(colored("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
-                              attrs=['dark']))
+    return check.is_true(response['success'],
+                         message='Error: {0}'.format(response['errorKey'] + errormessages))
 
-            self.failed = True
-            print(colored('ENSURE', 'red') +
-                  ' failed in ' + colored(self.name, attrs=['underline']) +
-                  ' for test ' + colored(self.current, attrs=['underline']))
-            print(colored(errormessage, attrs=['bold']))
 
-            # code for geting debug-info gratefully borrowed from
-            # https://stackoverflow.com/questions/6810999/how-to-determine-file-function-and-line-number
-            callerframerecord = inspect.stack()[calldepth]
-            frame = callerframerecord[0]
-            info = inspect.getframeinfo(frame)
-            print(colored('\n  File:     {0}'.format(info.filename), attrs=['dark']))
-            print(colored('  Function: {0}'.format(info.function), attrs=['dark']))
-            print(colored('  Line:     {0}'.format(info.lineno), attrs=['dark']))
-            print(colored("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━",
-                          attrs=['dark']))
-            return True
+def ensureError(response, check):
+    if not ensureValidResponse(response, check):
+        return False
+    return check.is_false(response['success'],
+                          message='Server responds success on illegal action.')
 
-    def ensureValidResponse(self, response, calldepth=1):
-        return self.ensure(response is not None, 'Invalid response. Likely a 404 or a stacktrace.', calldepth=calldepth)
 
-    def ensureSuccess(self, response):
-        if not self.ensureValidResponse(response):
-            return False
+def ensureErrorWithKey(response, check, errorKey):
+    if not ensureValidResponse(response, check):
+        return False
+    return check.is_true(response['success'] is False and response['errorKey'] == errorKey,
+                         message='Server did not respond correct errorKey.\n'
+                                 'Expected: {0}\n'
+                                 'Actual:   {1}\n'.format(errorKey, response[errorKey]))
 
-        errormessages = ''
 
-        if response is not None:
-            for message in response['errorProperties']:
-                errormessages += '\nMessage:  ' + message
+def ensureNotAuthorized(response, check):
+    if not ensureValidResponse(response, check):
+        return False
+    return check.is_true(response['success'] is False and response['errorKey'] == 'NotFound',
+                         message='Server did not respond with NotFound. Got "' + response['errorKey'] + '"\n')
 
-        self.ensure(response['success'] is True,
-                    errormessage='Error: {0}'.format(response['errorKey'] + errormessages),
-                    calldepth=2)
-        return response['success'] is True
 
-    def ensureError(self, response):
-        if not self.ensureValidResponse(response, calldepth=3):
-            return False
-        return self.ensure(response['success'] is False,
-                           errormessage='Server responds success on illegal action.',
-                           calldepth=2)
+def ensureNoData(response, check):
+    if not ensureValidResponse(response, check):
+        return False
+    return check.is_true('data' not in response or response['data'] is None,
+                         message='Data was returned when it should not have been.\n')
 
-    def ensureErrorWithKey(self, response, errorKey):
-        if not self.ensureValidResponse(response, calldepth=3):
-            return False
-        return self.ensure(response['success'] is False and response['errorKey'] == errorKey,
-                           errormessage='Server did not respond correct errorKey or success-flag',
-                           calldepth=2)
 
-    def ensureNotAuthorized(self, response):
-        if not self.ensureValidResponse(response, calldepth=3):
-            return False
-        return self.ensure(response['success'] is False and response['errorKey'] == 'NotFound',
-                           errormessage='Server did not respond with NotFound. Got "' + response['errorKey'] + '"',
-                           calldepth=2)
-
-    def ensureNoData(self, response):
-        if not self.ensureValidResponse(response, calldepth=3):
-            return False
-        return self.ensure('data' not in response or response['data'] is None,
-                           errormessage='Data was returned when it should not have been.',
-                           calldepth=2)
-
-    def ensureSomeData(self, response):
-        if not self.ensureValidResponse(response, calldepth=3):
-            return False
-        return self.ensure('data' in response and response['data'] is not None,
-                           errormessage='Data expected but none returned',
-                           calldepth=2)
-
-    def ensureEqual(self, expected, actual):
-        self.ensure(expected == actual,
-                    errormessage='Expected: {0}\nActual:   {1}'.format(expected, actual),
-                    calldepth=2)
-
-    def ensureNotEqual(self, expected, actual):
-        self.ensure(expected != actual,
-                    errormessage='Expected: {0}\nActual:   {1}'.format(expected, actual),
-                    calldepth=2)
-
-    def fails(self, errormessage, calldepth=2):
-        self.ensure(False, errormessage=errormessage, calldepth=calldepth + 1)
+def ensureSomeData(response, check):
+    if not ensureValidResponse(response, check):
+        return False
+    return check.is_true('data' in response and response['data'] is not None,
+                         message='Data expected, but none returned\n')
