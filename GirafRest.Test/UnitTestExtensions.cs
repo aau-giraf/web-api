7674fa32f22d6f01c9ac2ad8733e14cbebd6674a
Mathias Ibsen <mathias@ibsen.name>
2018-04-30 15:52:52 +0200

    Add check for duplicate weekschedule names

--- GirafRest.Test/UnitTestExtensions.cs
+++ GirafRest.Test/UnitTestExtensions.cs
@@ -218,6 +218,7 @@ namespace GirafRest.Test
                             },
                             new Week(){
                                 Id = 1,
+                                Name = "My not so awesome week",
                                 Weekdays = new List<Weekday>(){
                                     new Weekday(){
                                         Day = Days.Monday
@@ -244,6 +245,7 @@ namespace GirafRest.Test
                             },
                         };
                         MockUsers[0].WeekSchedule.Add(mockWeeks[0]);
+                        MockUsers[0].WeekSchedule.Add(mockWeeks[1]);
                         MockUsers[1].WeekSchedule.Add(mockWeeks[1]);
                         MockUsers[2].WeekSchedule.Clear();
                         MockUsers[3].WeekSchedule.Clear();
