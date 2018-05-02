b30cd162c64b2cb87be93080f110b80a981a1bc2
AsgerHB <asgerhb@gmail.com>
2018-05-02 11:40:25 +0200

    GET v1/weektemplate/ includes ID so you can get individual templates

--- GirafRest/Controllers/WeekTemplateController.cs
+++ GirafRest/Controllers/WeekTemplateController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GirafRest.Models;
 using GirafRest.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -43,14 +44,18 @@ namespace GirafRest.Controllers
             {
                 return new ErrorResponse<IEnumerable<WeekTemplateNameDTO>>(ErrorCode.UserNotFound);
             }
-            if (!user.WeekSchedule.Any())
+            
+            var result = _giraf._context.WeekTemplates
+                             .Where(t => t.DepartmentKey == user.DepartmentKey)
+                             .Select(w => new WeekTemplateNameDTO(w.Name, w.Id)).ToArray();
+            
+            if (result.Length < 1)
             {
                 return new ErrorResponse<IEnumerable<WeekTemplateNameDTO>>(ErrorCode.NoWeekTemplateFound);
             }
             else
             {
-                return new Response<IEnumerable<WeekTemplateNameDTO>>(_giraf._context.WeekTemplates.Include(w => w.Thumbnail).Include(u => u.Weekdays)
-                     .ThenInclude(wd => wd.Activities).Where(t => t.DepartmentKey == user.DepartmentKey).Select(w => new WeekTemplateNameDTO(w.Name)));
+                return new Response<IEnumerable<WeekTemplateNameDTO>>(result);
             }
         }
 
@@ -63,14 +68,19 @@ namespace GirafRest.Controllers
         public async Task<Response<WeekTemplateDTO>> GetWeekTemplate(long id)
         {
             var user = await _giraf.LoadUserAsync(HttpContext.User);
-            var week = await (_giraf._context.WeekTemplates.Include(w => w.Thumbnail).Include(u => u.Weekdays)
-                    .ThenInclude(wd => wd.Activities).Where(t => t.DepartmentKey == user.DepartmentKey).FirstOrDefaultAsync(w => w.Id == id));
+            
+            var week = await (_giraf._context.WeekTemplates
+                .Include(w => w.Thumbnail)
+                .Include(u => u.Weekdays)
+                    .ThenInclude(wd => wd.Activities)
+                .Where(t => t.DepartmentKey == user.DepartmentKey)
+                .FirstOrDefaultAsync(w => w.Id == id));
+            
             if (week != null)
-            {
                 return new Response<WeekTemplateDTO>(new WeekTemplateDTO(week));
-            }
             else
-                return new ErrorResponse<WeekTemplateDTO>(ErrorCode.WeekScheduleNotFound);
+                return new ErrorResponse<WeekTemplateDTO>(ErrorCode.WeekTemplateNotFound);
+        }
         }
     }
 }
