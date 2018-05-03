cc5e475e906f416895b132cab13b632638479b1d
philiihilihli <you@example.com>
2018-05-03 20:26:40 +0200

    Fixed typo in readme and updated migratatte.sql to work with actual produktion

--- scripts/migrate.sql
+++ scripts/migrate.sql
@@ -30,15 +30,26 @@ INSERT INTO AspNetRoles SELECT * FROM giraf_old.AspNetRoles;
 INSERT INTO AspNetUserRoles SELECT * FROM giraf_old.AspNetUserRoles;
 
 INSERT INTO Pictograms(id, Discriminator, LastEdit, AccessLevel, Image, Title, Sound) SELECT * FROM giraf_old.Frames WHERE Discriminator = "Pictogram";
+UPDATE Pictograms SET Image = TO_BASE64(Image);
 
 INSERT INTO Weeks SELECT id, Discriminator, GirafUserId, Name, ThumbnailKey, DepartmentKey FROM giraf_old.Weeks;
 INSERT INTO Weekdays SELECT * FROM giraf_old.Weekdays;
 
 DROP DATABASE giraf_old;
 
--- # Insert one guardian more, named Gorm
--- INSERT INTO `LauncherOptions` (`Key`, `activitiesCount`, `appGridSizeColumns`, `appGridSizeRows`, `checkResourceAppearence`, `defaultTimer`, `displayLauncherAnimations`, `orientation`, `theme`, `timerSeconds`) VALUES
--- (6, NULL,   NULL,   NULL,   1,  2,  CONV('0', 2, 10) + 0,   1,  1,  NULL);
--- INSERT INTO `AspNetUsers` (`Id`, `AccessFailedCount`, `ConcurrencyStamp`, `DepartmentKey`, `DisplayName`, `Email`, `EmailConfirmed`, `IsDepartment`, `LockoutEnabled`, `LockoutEnd`, `NormalizedEmail`, `NormalizedUserName`, `PasswordHash`, `PhoneNumber`, `PhoneNumberConfirmed`, `SecurityStamp`, `SettingsKey`, `TwoFactorEnabled`, `UserIcon`, `UserName`) VALUES
--- ('b2466bcd-14fd-4f56-a053-b133908fff44',    0,  '58765e22-fbec-45f8-bb54-0ad7f663dddc', 1,  NULL,   NULL,   CONV('0', 2, 10) + 0,   CONV('0', 2, 10) + 0,   CONV('1', 2, 10) + 0,   NULL,   NULL,   'GORM', 'AQAAAAEAACcQAAAAEOtxV3MLz7Pu6MPtE54xKvK+GuipWwm6RM14lPBa2Sm9UttwHIs4YWZ6GpyWPjhtww==', NULL,   CONV('0', 2, 10) + 0,   '8c31711a-a06d-4dca-9ff9-a7283e4f3ec9', 6,  CONV('0', 2, 10) + 0,   NULL,   'Gorm');
--- INSERT INTO AspNetUserRoles values("b2466bcd-14fd-4f56-a053-b133908fff44", "dce9d3c7-0ca1-4fb5-915d-c18cb902e12e");
+Delete from Weekdays where WeekId in (select Id from Weeks where ThumbnailKey in (select Id from Pictograms WHERE Image is null));
+Delete from Weeks where ThumbnailKey in (select Id from Pictograms WHERE Image is null);
+Delete from Pictograms WHERE Image is null;
+
+-- Insert department: birken
+-- TODO
+
+-- Insert one guardian more, named pædagog1, as a part of birken
+INSERT INTO `LauncherOptions` (`Key`, `activitiesCount`, `appGridSizeColumns`, `appGridSizeRows`, `checkResourceAppearence`, `defaultTimer`, `displayLauncherAnimations`, `orientation`, `theme`, `timerSeconds`) VALUES
+(6, NULL,   NULL,   NULL,   1,  2,  CONV('0', 2, 10) + 0,   1,  1,  NULL);
+INSERT INTO `AspNetUsers` (`Id`, `AccessFailedCount`, `ConcurrencyStamp`, `DepartmentKey`, `DisplayName`, `Email`, `EmailConfirmed`, `IsDepartment`, `LockoutEnabled`, `LockoutEnd`, `NormalizedEmail`, `NormalizedUserName`, `PasswordHash`, `PhoneNumber`, `PhoneNumberConfirmed`, `SecurityStamp`, `SettingsKey`, `TwoFactorEnabled`, `UserIcon`, `UserName`) VALUES
+('b2466bcd-14fd-4f56-a053-b133908fff44',    0,  '58765e22-fbec-45f8-bb54-0ad7f663dddc', 1,  NULL,   NULL,   CONV('0', 2, 10) + 0,   CONV('0', 2, 10) + 0,   CONV('1', 2, 10) + 0,   NULL,   NULL,   'BIRKEN', 'AQAAAAEAACcQAAAAEOtxV3MLz7Pu6MPtE54xKvK+GuipWwm6RM14lPBa2Sm9UttwHIs4YWZ6GpyWPjhtww==', NULL,   CONV('0', 2, 10) + 0,   '8c31711a-a06d-4dca-9ff9-a7283e4f3ec9', 6,  CONV('0', 2, 10) + 0,   NULL,   'birken');
+INSERT INTO AspNetUserRoles values("b2466bcd-14fd-4f56-a053-b133908fff44", "dce9d3c7-0ca1-4fb5-915d-c18cb902e12e");
+
+-- Insert citizens in birken
+-- TODO
