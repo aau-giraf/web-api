a633b8c4efa4a83ca79bf21cf105b0a18a697ffb
Morten Rask <morten.rask@live.dk>
2018-05-09 16:19:59 +0200

    Add departments to admin panel

--- GirafRest/Models/DTOs/UserNameDTO.cs
+++ GirafRest/Models/DTOs/UserNameDTO.cs
@@ -3,14 +3,14 @@
     public class UserNameDTO
     {
         public string UserName { get; set; }
-        public GirafRoles UserRole { get; private set; }
+        public string UserRole { get; private set; }
         public string UserId { get; set; }
 
 
         public UserNameDTO(string username, GirafRoles userRole, string userId)
         {
             UserName = username;
-            UserRole = userRole;
+            UserRole = userRole.ToString();
             UserId = userId;
         }
 
