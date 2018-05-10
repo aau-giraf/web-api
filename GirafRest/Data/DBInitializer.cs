90f6732148c38c60d28265b15f5ffd229edd6d8d
AsgerHB <asgerhb@gmail.com>
2018-05-10 15:40:47 +0200

    Whopie doo fix line that breaks everything

--- GirafRest/Data/DBInitializer.cs
+++ GirafRest/Data/DBInitializer.cs
@@ -106,7 +106,7 @@ namespace GirafRest.Setup
             {
                 new GirafUser("Kurt", departments[0]),
                 new GirafUser("Graatand", departments[0]),
-                new GirafUser("Lee", null),
+                new GirafUser{UserName = "Lee"},
                 new GirafUser("Tobias", departments[0]),
                 new GirafUser("Decker", departments[0])
             };
