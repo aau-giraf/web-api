e52e00689a487e8b023795e68155627068264160
Mathias Ibsen <mathias@ibsen.name>
2018-05-16 14:52:57 +0200

    Make foreignkey from week to girafuser required

---
+++ GirafRest/Migrations/20180516124710_MakeGirafUserIdOnWeekRequired.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GirafRest.Migrations
+{
+    public partial class MakeGirafUserIdOnWeekRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GirafUserId",
+                table: "Weeks",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GirafUserId",
+                table: "Weeks",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
