7fe7da20f07bfdb6e430e631744fbe8674fec7a3
KVISDAOWNER <krijen16@student.aau.dk>
2019-05-01 12:23:25 +0200

    Added ActivityControllerTest.cs

---
+++ GirafRest.Test/Controllers/ActivityControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GirafRest.Controllers;
+using GirafRest.Models;
+using GirafRest.Models.DTOs;
+using GirafRest.Models.Responses;
+using GirafRest.Services;
+using GirafRest.Test.Mocks;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+using static GirafRest.Test.UnitTestExtensions;
+
+namespace GirafRest.Test.Controllers
+{
+    class ActivityControllerTest
+    {
+        private TestContext _testContext;
+
+        private ActivityController initializeTest()
+        {
+            _testContext = new TestContext();
+
+            var wc = new ActivityController(
+                new MockGirafService(_testContext.MockDbContext.Object,
+                                     _testContext.MockUserManager), _testContext.MockLoggerFactory.Object,
+                new GirafAuthenticationService(_testContext.MockDbContext.Object, _testContext.MockRoleManager.Object,
+                                               _testContext.MockUserManager));
+            _testContext.MockHttpContext = wc.MockHttpContext();
+
+            return wc;
+        }
+    }
+}
