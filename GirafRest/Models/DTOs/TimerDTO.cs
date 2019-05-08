1f29fe5fd16b7e1789f91429000cc1f9c5567d8e
Mads Stenkær <mstenk16@student.aau.dk>
2019-05-08 13:45:39 +0200

    DTO for timer

---
+++ GirafRest/Models/DTOs/TimerDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace GirafRest.Models.DTOs
+{
+    public class TimerDTO
+    {
+        public TimerDTO(DateTime startTime, long progress, long fullLength, bool paused)
+        {
+            StartTime = startTime;
+            Progress = progress;
+            FullLength = fullLength;
+            Paused = paused;
+        }
+
+        public TimerDTO(Timer timer) { 
+            StartTime = timer.StartTime;
+            Progress = timer.Progress;
+            FullLength = timer.FullLength;
+            Paused = timer.Paused;
+        }
+
+        public TimerDTO() { }
+
+        /// <summary>
+        /// Start time of the timer
+        /// </summary>
+        public DateTime StartTime {get; set;}
+
+        /// <summary>
+        /// The progress of the timer in miliseconds.
+        /// </summary>
+        public long Progress {get; set;}
+
+        /// <summary>
+        /// The full length of the timer.
+        /// </summary>
+        public long FullLength {get; set;}
+
+        /// <summary>
+        /// Boolean for setting whether the timer is paused.
+        /// </summary>
+        public bool Paused {get; set;}
+    }
+}
