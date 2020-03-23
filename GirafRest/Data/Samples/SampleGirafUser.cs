3e807127d6d4f2813f477ec5c0d5e64abcb659a5
aerlin17 <34278730+aerlin17@users.noreply.github.com>
2020-03-23 15:24:47 +0100

    a

---
+++ GirafRest/Data/Samples/SampleGirafUser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace GirafRest.Setup
+{
+    public class SampleGirafUser
+    {
+        public string Name { get; set; }
+        public long DepKey { get; set; }
+        public string Role { get; set; }
+        public List<string> Weeks { get; set; }
+        public string Password { get; set; }
+
+        public SampleGirafUser(string name, long depKey, string role, List<string> weeks, string password)
+        {
+            Name = name;
+            DepKey = depKey;
+            Role = role;
+            Weeks = weeks;
+            Password = password;
+        }
+    }
+}
---
+++ GirafRest/Data/Samples/SampleWeekTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace GirafRest.Setup
+{
+    public class SampleWeekTemplate
+    {
+        public string Name { get; set; }
+        public long PictKey { get; set; }
+        public long DepartmentKey { get; set; }
+
+        public SampleWeekTemplate(string name, long pictKey, long departmentKey)
+        {
+            Name = name;
+            PictKey = pictKey;
+            DepartmentKey = departmentKey;
+        }
+    }  
+}
