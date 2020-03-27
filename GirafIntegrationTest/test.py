398a845ca16d0f346724f6f683ef00e455b875c9
Badgy <jonaskhansen@gmail.com>
2020-03-27 10:02:12 +0100

    use python instead of bash for launch

---
+++ GirafIntegrationTest/test.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.runner import TextTestRunner
+from testlib import GIRAFTestResults, GIRAFTestRunner, compare
+
+
+runner = GIRAFTestRunner(verbosity=5, tb_locals=True, resultclass=GIRAFTestResults)
+unittest.defaultTestLoader.sortTestMethodsUsing = compare
+suite = unittest.defaultTestLoader.discover(start_dir='tests')
+
+if __name__ == '__main__':
+    print('\033[33m' + 'Running integration tests for the GIRAF web API' + '\033[0m')
+    print('----------------------------------------------------------------------\n')
+    runner.run(suite)
