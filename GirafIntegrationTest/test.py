df2073b8876fba476bb5c472630b5695e79e2393
Badgy <jonaskhansen@gmail.com>
2020-03-29 14:34:02 +0200

    add error message when server is unavailable

--- GirafIntegrationTest/test.py
+++ GirafIntegrationTest/test.py
@@ -1,5 +1,13 @@
 import unittest
-from testlib import GIRAFTestResults, GIRAFTestRunner, compare
+from requests import get
+from requests.exceptions import ConnectionError
+from testlib import GIRAFTestResults, GIRAFTestRunner, compare, BASE_URL
+
+try:
+    result = get(f'{BASE_URL}v1/Error').json()
+except ConnectionError:
+    print('\033[91m' + 'Error:' + '\033[0m' + ' could not get response from server.\nExiting...')
+    exit(1)
 
 # setup runner with high verbosity, variable printing on fail/error, and custom results class
 runner = GIRAFTestRunner(verbosity=5, tb_locals=True, resultclass=GIRAFTestResults)
