4ad871f14a84ca4e92e42044205434e5654f5357
MrPekar98 <martin.pekar@outlook.dk>
2020-04-13 09:17:33 +0200

    New Setting attribute.

--- GirafRest/Migrations/GirafDbContextModelSnapshot.cs
+++ GirafRest/Migrations/GirafDbContextModelSnapshot.cs
@@ -240,6 +240,8 @@ namespace GirafRest.Migrations
 
                     b.Property<int?>("TimerSeconds");
 
+                    b.Property<int?>("CompletedActivityOption");
+
                     b.HasKey("Key");
 
                     b.ToTable("Setting");
