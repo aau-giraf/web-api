f9910a0e4c6b9c8c1e84796fc47ea4784a975643
MrPekar98 <martin.pekar@outlook.dk>
2020-04-13 09:49:12 +0200

    Tests new Setting field.

--- GirafRest.Test/Controllers/UserControllerTest.cs
+++ GirafRest.Test/Controllers/UserControllerTest.cs
@@ -71,7 +71,8 @@ namespace GirafRest.Test
                 DefaultTimer = DefaultTimer.analogClock,
                 ActivitiesCount = 5,
                 NrOfDaysToDisplay = 5,
-                GreyScale = true}
+                GreyScale = true,
+                CompletedActivityOption = 2}
             };
 
             return usercontroller;
@@ -986,6 +987,7 @@ namespace GirafRest.Test
             Assert.Equal(5, _testContext.MockUsers[CITIZEN_DEP_TWO].Settings.ActivitiesCount);
             Assert.Equal(5, _testContext.MockUsers[CITIZEN_DEP_TWO].Settings.NrOfDaysToDisplay);
             Assert.True(_testContext.MockUsers[CITIZEN_DEP_TWO].Settings.GreyScale);
+            Assert.True(2, _testContext.MockUsers[CITIZEN_DEP_TWO].Settings.CompletedActivityOption);
         }
 
         [Theory]
