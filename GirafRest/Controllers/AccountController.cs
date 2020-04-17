91d2d30db3aa3447901b2184a4c2185db9ad273e
Seraph <seraph14@hotmail.com>
2020-04-17 13:32:36 +0200

    revert

--- GirafRest/Controllers/AccountController.cs
+++ GirafRest/Controllers/AccountController.cs
@@ -81,7 +81,6 @@ namespace GirafRest.Controllers
                 return new ErrorResponse<string>(ErrorCode.InvalidCredentials);
 
             var loginUser = _giraf._context.Users.FirstOrDefault(u => u.UserName == model.Username);
-
             return new Response<string>(await GenerateJwtToken(loginUser, loginUser.Id));
 
         }
