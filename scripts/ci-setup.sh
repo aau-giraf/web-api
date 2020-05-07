92fef6208c17389c8315bbfb792ea873c255ab0a
Badgy <jonaskhansen@gmail.com>
2020-05-07 10:51:04 +0200

    use localhost

--- scripts/ci-setup.sh
+++ scripts/ci-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 default_con="server=<db-host>;port=<db-port>;userid=<db-user>;password=<db-password>;database=<db-db>;Allow User Variables=True"
-new_con="server=127.0.0.1;userid=$1;password=$2;database=giraf;Allow User Variables=True"
+new_con="server=localhost;userid=$1;password=$2;database=giraf;Allow User Variables=True"
 jwt_key="kfrj2fjn90f93nhf93urfg93urgb93urgdfklgnhurbn894hbg8u4brg84brg84rhg487rhg84rhng8unv8r4nv48urhf8u4hrf"
 jwt_iss="AAU"
 limit=2000
