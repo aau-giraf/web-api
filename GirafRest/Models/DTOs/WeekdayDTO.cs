5fd658d22b8b09bb644e3122f1d8ef31988810c8
Christopher Kirkegaard Sørensen <chrisksorensen@gmail.com>
2020-05-12 14:54:16 +0200

    Update WeekdayDTO.cs

--- GirafRest/Models/DTOs/WeekdayDTO.cs
+++ GirafRest/Models/DTOs/WeekdayDTO.cs
@@ -25,6 +25,9 @@ namespace GirafRest.Models.DTOs
         /// </summary>
         /// <param name="weekday">The weekday to create a DTO for.</param>
         public WeekdayDTO(Weekday weekday) {
+            if (weekday == null) {
+                throw new System.ArgumentNullException(weekday + " is null");
+            }
             this.Day = weekday.Day;
             Activities = new List<ActivityDTO>();
             
