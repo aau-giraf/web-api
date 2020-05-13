2ec202f5b210a170c560a7129995a99011985ee0
Christopher Kirkegaard Sørensen <chrisksorensen@gmail.com>
2020-05-13 08:33:26 +0200

    Update 20200422090502_PictogramtextMigration.Designer.cs

--- GirafRest/Migrations/20200422090502_PictogramtextMigration.Designer.cs
+++ GirafRest/Migrations/20200422090502_PictogramtextMigration.Designer.cs
@@ -12,6 +12,7 @@ namespace GirafRest.Migrations
     [Migration("20200422090502_PictogramtextMigration")]
     partial class PictogramtextMigration
     {
+        ///
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
