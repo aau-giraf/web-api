b5a41c6246057261bcc2a09cd99674a7fa6d15b0
Frederik Spang <frederik@progras.dk>
2020-05-14 11:25:18 +0200

    Fixes relative path

--- GirafRest/Setup/Startup.cs
+++ GirafRest/Setup/Startup.cs
@@ -236,7 +236,7 @@ namespace GirafRest.Setup
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Giraf REST API V1");
+                c.SwaggerEndpoint("v1/swagger.json", "Giraf REST API V1");
                 c.DocExpansion(DocExpansion.None);
             });
 
