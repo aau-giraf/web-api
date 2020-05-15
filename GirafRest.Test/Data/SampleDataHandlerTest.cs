c336a2a190aa67dcbd467a64bbc1a977e6208289
Markus Jeppesen <mjep17@student.aau.dk>
2020-05-15 14:34:50 +0200

    changes to controllers

--- GirafRest.Test/Data/SampleDataHandlerTest.cs
+++ GirafRest.Test/Data/SampleDataHandlerTest.cs
@@ -53,33 +53,19 @@ namespace GirafRest.Test
             List<SampleWeekTemplate> weekTemplates = data.WeekTemplateList;
             List<SampleWeekday> weekdays = data.WeekdayList;
 
-            String str1 = new String("Admin");
-            String str2 = new String("Guardian in dep 2");
-            String str3 = new String("Citizen of dep 2");
-            String str4 = new String("Mock Department");
-            String str5 = new String("Mock Department2");
-            String str6 = new String("Picto 1");
-            String str7 = new String("Public Picto2");
-            String str8 = new String("No restrictions");
-            String str9 = new String("My awesome week");
-            String str10 = new String("My not so awesome week");
-            String str11 = new String("Template1");
-            String str12 = new String("Template2");
-            
-
             Assert.NotNull(data);
-            Assert.Equal(users[0].Name, str1);
-            Assert.Equal(users[1].Name, str2);
-            Assert.Equal(users[2].Name, str3);
-            Assert.Equal(deps[0].Name, str4);
-            Assert.Equal(deps[1].Name, str5);
-            Assert.Equal(pics[0].Title, str6);
-            Assert.Equal(pics[1].Title, str7);
-            Assert.Equal(pics[2].Title, str8);
-            Assert.Equal(weeks[0].Name, str9);
-            Assert.Equal(weeks[1].Name, str10);
-            Assert.Equal(weekTemplates[0].Name, str11);
-            Assert.Equal(weekTemplates[1].Name, str12);
+            Assert.Equal("Admin", users[0].Name);
+            Assert.Equal("Guardian in dep 2", users[1].Name);
+            Assert.Equal("Citizen of dep 2",users[2].Name);
+            Assert.Equal("Mock Department", deps[0].Name);
+            Assert.Equal("Mock Department2", deps[1].Name);
+            Assert.Equal("Picto 1", pics[0].Title);
+            Assert.Equal("Public Picto2", pics[1].Title);
+            Assert.Equal("No restrictions", pics[2].Title);
+            Assert.Equal("My awesome week", weeks[0].Name);
+            Assert.Equal("My not so awesome week", weeks[1].Name);
+            Assert.Equal("Template1",weekTemplates[0].Name);
+            Assert.Equal("Template1", weekTemplates[1].Name);
             Assert.Equal(Day.Monday, weekdays[0].Day);
             Assert.Equal(Day.Tuesday, weekdays[1].Day);
             Assert.Equal(Day.Wednesday, weekdays[2].Day);
