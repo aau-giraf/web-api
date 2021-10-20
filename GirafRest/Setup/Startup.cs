4f3064a3ba2bdc8ef92487f96a4b72725fc06ca6
Drejer1 <63230715+Drejer1@users.noreply.github.com>
2021-10-20 08:25:09 +0200

    Update Startup.cs

--- GirafRest/Setup/Startup.cs
+++ GirafRest/Setup/Startup.cs
@@ -129,7 +129,7 @@ namespace GirafRest.Setup
             services.AddControllers().AddNewtonsoftJson();
             //add dbcontext
             services.AddEntityFrameworkMySql().AddDbContext<GirafDbContext>(options => options.UseMySql("name=ConnectionStrings:DefaultConnection"));
-            //add repositories
+            //add scoped repositories. Every single request gets it's own scoped repositories.
             services.AddScoped<IAlternateNameRepository,AlternateNameRepository>();
             services.AddScoped<IDepartmentRepository,DepartmentRepository>();
             services.AddScoped<IGirafRoleRepository, GirafRoleRepository>();
