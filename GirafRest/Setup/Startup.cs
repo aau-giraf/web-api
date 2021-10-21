3aabbd3d9b4218bf41e0233afdd9d9dd62b2eb0b
Drejer1 <63230715+Drejer1@users.noreply.github.com>
2021-10-21 14:47:19 +0200

    added weekday repositories

--- GirafRest/Setup/Startup.cs
+++ GirafRest/Setup/Startup.cs
@@ -147,6 +147,7 @@ namespace GirafRest.Setup
             services.AddScoped<IPictogramRelationRepository,PictogramRelationRepository>();
             services.AddScoped<IUserResourseRepository, UserResourseRepository>();
             services.AddScoped<IImageRepository, ImageRepository>();
+            services.AddScoped<IWeekdayRepository, WeekdayRepository>();
            
             // Set up Cross-Origin Requests
             services.AddCors(o => o.AddPolicy("AllowAll", builder =>
