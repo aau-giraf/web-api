d6ca988da15c793c858c88247f3143a77683e055
goldflue <45980653+goldflue@users.noreply.github.com>
2021-11-03 14:55:40 +0100

    Added further tests for AlternateNameController

--- GirafRest/IRepositories/IAlternateNameRepository.cs
+++ GirafRest/IRepositories/IAlternateNameRepository.cs
@@ -12,8 +12,17 @@ namespace GirafRest.IRepositories
         ///   if no name was found for the <see cref="GirafUser"/> and <see cref="Pictogram"/>
         /// </summary>
         /// <param name="userId">The ID of the <see cref="GirafUser"/> which has the <see cref="AlternateName"/> for the <see cref="Pictogram"/></param>
-        /// <param name="PictogramId">The ID of the <see cref="Pictogram"/></param>
+        /// <param name="pictogramId">The ID of the <see cref="Pictogram"/></param>
         /// <returns><see cref="AlternateName"/> or default, if not found</returns>
-        AlternateName GetForUser(string userId, long PictogramId);
+        AlternateName GetForUser(string userId, long pictogramId);
+
+        /// <summary>
+        ///  If the <see cref="AlternateName"/> has already been created, it will return true.
+        /// </summary>
+        /// <param name="userId">The ID of the <see cref="GirafUser"/> which has the <see cref="AlternateName"/> for the <see cref="Pictogram"/></param>
+        /// <param name="pictogramId">The ID of the <see cref="Pictogram"/></param>
+        /// <returns>boolean</returns>
+        bool UserAlreadyHas(string userId, long pictogramId)
+            => GetForUser(userId, pictogramId) != null;
     }
 }
