459972529c02095c4cba9d8fd5c347d764521c85
Molitany <thor@beregaard.dk>
2021-11-04 09:10:28 +0100

    Added ordering and extensions for common functions and more tests and "mock" that is not really mock but i dont know what to call it

--- GirafRest.IntegrationTest/CustomWebApplicationFactory.cs
+++ GirafRest.IntegrationTest/CustomWebApplicationFactory.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Linq;
 using GirafRest.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -16,64 +13,21 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using GirafRest.Setup;
 
 namespace GirafRest.IntegrationTest
 {
-    public class CustomWebApplicationFactory<TStartup>
-    : WebApplicationFactory<TStartup> where TStartup : class
+    public class CustomWebApplicationFactory
+    : WebApplicationFactory<Startup>
     {
-        /// <summary>
-        /// The configuration, contains information regarding connecting to the database
-        /// </summary>
-        private IConfigurationRoot Configuration { get; }
-
-        UserManager<GirafUser> UserManager;
         protected override IHostBuilder CreateHostBuilder()
         {
             var builder = Host.CreateDefaultBuilder()
                               .ConfigureWebHostDefaults(x =>
                               {
-                                  x.UseStartup<Setup.Startup>().UseTestServer();
+                                  x.UseStartup<Startup>().UseTestServer();
                               });
             return builder;
-        }
-
-        /// <summary>
-        /// Configures the GirafUser Identity, changing what is needed by it, and how it should act
-        /// </summary>
-        /// <param name="services">A collection of all services in the application</param>
-        private void configureIdentity<T>(IServiceCollection services)
-            where T : GirafDbContext
-        {
-            //Add Identity for user management.
-            services.AddIdentity<GirafUser, GirafRole>(options =>
-            {
-                options.RemovePasswordRequirements();
-            })
-                .AddEntityFrameworkStores<T>()
-                .AddDefaultTokenProviders();
-
-            // Add Jwt Authentication
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
-            services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(cfg =>
-                {
-                    cfg.RequireHttpsMetadata = false;
-                    cfg.SaveToken = true;
-                    cfg.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidIssuer = Configuration["Jwt:JwtIssuer"],
-                        ValidAudience = Configuration["Jwt:JwtIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:JwtKey"])),
-                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
-                    };
-                });
         }
     }
 }
--- GirafRest.IntegrationTest/Tests/AccountControllerTest.cs
+++ GirafRest.IntegrationTest/Tests/AccountControllerTest.cs
@@ -2,43 +2,173 @@ using Xunit;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Text;
+using System;
+using Newtonsoft.Json.Linq;
+using GirafRest.IntegrationTest.Extensions;
+using GirafRest.IntegrationTest.Setup;
+using GirafRest.IntegrationTest.Order;
 
-namespace GirafRest.IntegrationTest
+namespace GirafRest.IntegrationTest.Tests
 {
+    [TestCaseOrderer("GirafRest.IntegrationTest.Order.PriorityOrderer", "GirafRest.IntegrationTest")]
+    [Collection("Account Controller")]
     public class AccountControllerTest
-    : IClassFixture<CustomWebApplicationFactory<Setup.Startup>>
+    : IClassFixture<CustomWebApplicationFactory>, IClassFixture<AccountMock>
     {
-        private readonly CustomWebApplicationFactory<Setup.Startup> _factory;
+        private readonly CustomWebApplicationFactory _factory;
+        private readonly AccountMock _accountMock;
         private const string BASE_URL = "https://localhost:5000/";
-
-        public AccountControllerTest(CustomWebApplicationFactory<Setup.Startup> factory)
+        private readonly HttpClient client;
+        public AccountControllerTest(CustomWebApplicationFactory factory, AccountMock accountMock)
         {
             _factory = factory;
+            client = _factory.CreateClient();
+            _accountMock = accountMock;
         }
-        
+
         /// <summary>
         ///Testing logging in as Guardian
         ///Endpoint: POST:/v2/Account/login
         /// </summary>
-        [Fact]
+        [Fact, Priority(0)]
         public async void TestAccountCanLoginAsGuardian()
         {
-            var client = _factory.CreateClient();
-            HttpContent content = new StringContent(JsonConvert.SerializeObject(new LoginDTO("Graatand", "password")),
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(new LoginDTO("Graatand", "password")),
                                     Encoding.UTF8,
                                     "application/json");
-            var response = await client.PostAsync($"{BASE_URL}v2/Account/login", content);
+            var response = await client.PostAsync($"{BASE_URL}v2/Account/login", httpContent);
+            string data = await response.Content.ReadAsStringAsync();
+            JObject content = JObject.Parse(data);
+            string token = content["data"].ToString();
 
             response.EnsureSuccessStatusCode();
-            Assert.NotEmpty(await response.Content.ReadAsStringAsync());
+            Assert.NotEmpty(data);
         }
 
-        [Fact]
-        public void test_account_can_get_guardian_id()
+        /// <summary>
+        /// Testing getting Guardian's id
+        /// Endpoint: GET:/v1/User
+        /// </summary>
+        [Fact, Priority(1)]
+        public async void TestAccountCanGetGuardianId()
         {
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{BASE_URL}v1/User/"),
+                Method = HttpMethod.Get
+            };
+            request.Headers.Add("Authorization", $"Bearer {await LoginExtension.GetTokenAsync(_factory)}");
 
+            var response = await client.SendAsync(request);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(content["data"].ToString());
+            Assert.NotEmpty(content["data"]["id"].ToString());
         }
 
+        /// <summary>
+        /// Testing registering a citizen fails without displayName
+        /// Endpoint: POST:/v2/Account/register
+        /// </summary>
+        [Fact, Priority(2)]
+        public async void TestAccountCannotRegisterCitizenWithoutDisplayName()
+        {
+            var data = "{'username': 'myUsername', 'password': 'password', 'role': 'Citizen', 'departmentId': 1}";
 
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{BASE_URL}v2/Account/register"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(data, Encoding.UTF8, "application/json"),
+            };
+            request.Headers.Add("Authorization", $"Bearer {await LoginExtension.GetTokenAsync(_factory)}");
+
+            var response = await client.SendAsync(request);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("MissingProperties", content["errorKey"]);
+        }
+
+        /// <summary>
+        /// Testing registering a citizen fails with empty displayName
+        /// Endpoint: POST:/v2/Account/register
+        /// </summary>
+        [Fact, Priority(3)]
+        public async void TestAccountCannotRegisterCitizenWithEmptyDisplayName()
+        {
+            var data = "{'username': 'myUsername', 'displayName': '', 'password': 'password'," +
+                "'role': 'Citizen', 'departmentId': 1}";
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{BASE_URL}v2/Account/register"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(data, Encoding.UTF8, "application/json"),
+            };
+            request.Headers.Add("Authorization", $"Bearer {await LoginExtension.GetTokenAsync(_factory)}");
+
+            var response = await client.SendAsync(request);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("MissingProperties", content["errorKey"]);
+        }
+
+        /// <summary>
+        /// Testing registering Citizen2
+        /// Endpoint: POST:/v2/Account/register
+        /// </summary>
+        [Fact, Priority(4)]
+        public async void TestAccountCanRegisterCitizen2()
+        {
+            var data = $"{{'username': '{_accountMock.Citizen2Username}', 'displayName': '{_accountMock.Citizen2Username}', 'password': 'password', 'role': 'Citizen', 'departmentId': 2}}";
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{BASE_URL}v2/Account/register"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(data, Encoding.UTF8, "application/json"),
+            };
+
+            request.Headers.Add("Authorization", $"Bearer {await LoginExtension.GetTokenAsync(_factory)}");
+
+            var response = await client.SendAsync(request);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+            Assert.NotEmpty(content["data"]);
+        }
+
+        /// <summary>
+        /// Testing logging in as Citizen2
+        /// Endpoint: POST:/v2/Account/login
+        /// </summary>
+        [Fact, Priority(5)]
+        public async void TestAccountCanLoginAsCitizen2()
+        {
+            var data = $"{{'username': '{_accountMock.Citizen2Username}', 'password': 'password'}}";
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri($"{BASE_URL}v2/Account/login"),
+                Method = HttpMethod.Post,
+                Content = new StringContent(data, Encoding.UTF8, "application/json"),
+            };
+
+            request.Headers.Add("Authorization", $"Bearer {await LoginExtension.GetTokenAsync(_factory)}");
+
+            var response = await client.SendAsync(request);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+            response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(content["data"].ToString());
+        }
     }
 }
