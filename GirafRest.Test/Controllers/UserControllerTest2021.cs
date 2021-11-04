0889ed66c366501135e2829672c004d22882b530
HowToYEET <kkapke19@student.aau.dk>
2021-11-04 12:37:26 +0100

    like dis??? istedet for at teste om den sortere liste så test compareTo
    ye

--- GirafRest.Test/Controllers/UserControllerTest2021.cs
+++ GirafRest.Test/Controllers/UserControllerTest2021.cs
@@ -1,27 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using GirafRest.Controllers;
-using System.Text;
+﻿
 using Xunit;
-using Moq;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
-using GirafRest.Extensions;
-using GirafRest.Models;
 using GirafRest.Models.DTOs;
-using GirafRest.Models.Responses;
-using GirafRest.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.InMemory;
-using GirafRest.Data;
-using GirafRest.Test.Mocks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace GirafRest.Test.Controllers
 {
@@ -29,34 +10,47 @@ namespace GirafRest.Test.Controllers
     public class UserControllerTest2021
     {
         readonly GirafRoles citizen = GirafRoles.Citizen;
-
-        [TestMethod]
-        public void GetSortedCitizens()
+       
+        [Fact]
+        public void CompareValueAfterCurrent()
         {
             //Arrange 
-            List<DisplayNameDTO> unsorted_test_list = new List<DisplayNameDTO>();
-            List<DisplayNameDTO> sorted_test_list = new List<DisplayNameDTO>();
+
             DisplayNameDTO Kenneth = new DisplayNameDTO("Kenneth", citizen, "1101");
             DisplayNameDTO Louise = new DisplayNameDTO("Louise", citizen, "1101");
-            DisplayNameDTO Josefine = new DisplayNameDTO("Josefine", citizen, "1101");
-            DisplayNameDTO Søren = new DisplayNameDTO("Søren", citizen, "1101");
-            DisplayNameDTO Fatima = new DisplayNameDTO("Fatima", citizen, "1101");
-            DisplayNameDTO Christoffer = new DisplayNameDTO("Christoffer", citizen, "1101");
-            //add users to list unsorted
-            unsorted_test_list.Add(Josefine);
-            unsorted_test_list.Add(Kenneth);
-            unsorted_test_list.Add(Louise);
-            unsorted_test_list.Add(Søren);
-            unsorted_test_list.Add(Fatima);
-            unsorted_test_list.Add(Christoffer);
+            int expected = -1;
             //Act
-            var sorted = unsorted_test_list.OrderBy(person => person.DisplayName); //Make an sorted version of the unsorted list
-            unsorted_test_list.Sort(); //sort the original list
-            
+            int actual = Kenneth.CompareTo(Louise);
             //Assert
-            CollectionAssert.AreEqual(sorted.ToList(), unsorted_test_list.ToList()); //check is they are equal in order
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
         }
 
+        [Fact]
+        public void CompareValueBeforeCurrent()
+        {
+            //Arrange 
+
+            DisplayNameDTO Kenneth = new DisplayNameDTO("Kenneth", citizen, "1101");
+            DisplayNameDTO Louise = new DisplayNameDTO("Louise", citizen, "1101");
+            int expected = 1;
+            //Act
+            int actual = Louise.CompareTo(Kenneth);
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+        }
+        [Fact]
+        public void CompareValueSameAsCurrent()
+        {
+            //Arrange 
+
+            DisplayNameDTO Kenneth = new DisplayNameDTO("Louise", citizen, "1101");
+            DisplayNameDTO Louise = new DisplayNameDTO("Louise", citizen, "1101");
+            int expected = 0;
+            //Act
+            int actual = Louise.CompareTo(Kenneth);
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+        }
     }
 
 }
