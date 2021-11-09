17d1b16fc9b5401a8212f13a3a4ff2aa4a3b2732
Simon <58163499+Sijmon@users.noreply.github.com>
2021-11-09 12:01:11 +0100

    Test (Repositories): Added repository tests

---
+++ GirafRest.Test/Repositories/GirafUserRepositoryTest.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using GirafRest.Data;
+using GirafRest.Models;
+using GirafRest.Models.DTOs;
+using GirafRest.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace GirafRest.Test.Repositories
+{
+    public class GirafUserRepositoryContext
+    {
+        protected GirafUserRepositoryContext(DbContextOptions<GirafDbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        protected DbContextOptions<GirafDbContext> ContextOptions { get; }
+
+        public void Seed()
+        {
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                //make new entities here
+                var user = new GirafUser()
+                {
+                    Id = "1",
+                    UserName = "user1"
+                };
+
+                context.Add(user);
+
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class GirafUserRepositoryTest : GirafUserRepositoryContext
+    {
+        private const string UserId = "1";
+        private const string Username = "user1";
+        public GirafUserRepositoryTest()
+            : base(new DbContextOptionsBuilder<GirafDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString("N")).Options)
+        { }
+
+        [Theory]
+        [InlineData(UserId)]
+        public void GetByIDTest(string id)
+        {
+            //Arrange
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                var repository = new GirafUserRepository(context);
+
+                //Act
+                var username = repository.GetByID(id).UserName;
+
+                //Assert
+                Assert.Equal(Username, username);
+            }
+        }
+    }
+}
---
+++ GirafRest.Test/Repositories/PictogramRepositoryTest.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using GirafRest.Data;
+using GirafRest.Models;
+using GirafRest.Models.DTOs;
+using GirafRest.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace GirafRest.Test.Repositories
+{
+    public class PictogramRepositoryContext
+    {
+        protected PictogramRepositoryContext(DbContextOptions<GirafDbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        protected DbContextOptions<GirafDbContext> ContextOptions { get; }
+
+        public void Seed()
+        {
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                //make new entities here
+                var user = new Pictogram()
+                {
+                    Id = 1,
+                    Title = "pic1"
+                };
+
+                context.Add(user);
+
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class PictogramRepositoryTest : PictogramRepositoryContext
+    {
+        private const int PicId = 1;
+        private const string PicTitle = "pic1";
+        public PictogramRepositoryTest()
+            : base(new DbContextOptionsBuilder<GirafDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString("N")).Options)
+        { }
+
+        [Theory]
+        [InlineData(PicId)]
+        public void GetByIDTest(int id)
+        {
+            //Arrange
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                var repository = new PictogramRepository(context);
+
+                //Act
+                var picTitle = repository.GetByID(id).Title;
+
+                //Assert
+                Assert.Equal(PicTitle, picTitle);
+            }
+        }
+    }
+}
