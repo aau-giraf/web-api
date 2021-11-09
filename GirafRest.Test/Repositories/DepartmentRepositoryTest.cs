c34c8993a70f98547e4c4ca9d7a3f3c60b398fac
MathiasNielsen1206 <56735904+MathiasNielsen1206@users.noreply.github.com>
2021-11-09 11:24:24 +0100

    Tests (DepartmentRepository): tested methods in department repository

---
+++ GirafRest.Test/Repositories/DepartmentRepositoryTest.cs
@@ -0,0 +1,76 @@
+﻿using Xunit;
+using System;
+using GirafRest.Data;
+using GirafRest.Models;
+using GirafRest.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace GirafRest.Test.Repositories
+{
+    public class FakeDepartmentRepositoryContext
+    {
+        protected FakeDepartmentRepositoryContext(DbContextOptions<GirafDbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+            Seed();
+        }
+        protected DbContextOptions<GirafDbContext> ContextOptions { get; }
+
+        public void Seed()
+        {
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var dep = new Department()
+                {
+                    Key = 1,
+                    Name = "dep1",
+                };
+                var dep2 = new Department()
+                {
+                    Key = 2,
+                    Name = "dep1",
+                };
+                var dep3 = new Department()
+                {
+                    Key = 3,
+                    Name = "dep1",
+                };
+                
+                context.AddRange(dep,dep2,dep3);
+                context.SaveChanges();
+
+            }
+        }
+    }
+    
+    public class DepartmentRepositoryTest : FakeDepartmentRepositoryContext
+    {
+        private const long DEP_1 = 1;
+        private const string DEP1_NAME = "dep1";
+        public DepartmentRepositoryTest()
+            : base(new DbContextOptionsBuilder<GirafDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString("N")).Options){}
+
+        [Theory]
+        [InlineData(DEP_1)]
+        public void GetDepartmentById_Test(long depKey)
+        {   // Arrange
+            using (var context = new GirafDbContext(ContextOptions))
+            {
+                var repository = new DepartmentRepository(context);
+                
+                // Act
+                var response = repository.GetDepartmentById(depKey);
+                
+                // Assert 
+                Assert.Equal(depKey,response.Key);
+                Assert.Equal(DEP1_NAME,response.Name);
+            }
+        }
+    }
+}
