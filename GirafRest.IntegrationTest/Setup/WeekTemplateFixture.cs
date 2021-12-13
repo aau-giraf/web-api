f961060b5885372a01586504a621573da96f05a4
aThir <thor@beregaard.dk>
2021-12-13 12:54:40 +0100

    Integration test (#238)
    * Fix enum
    
    * Added Integration tests
    
    * Finished failing tests
    
    * Increased ratelimit
    
    * New setup with the new dev data
    
    * Made it run the specific tests
    
    * IntegrationTests workfile remade to dotnet
    
    * FIXED
    
    * Refactoring
    
    Refactored
    
    * Update
    
    Update
    
    * Appropriation
    
    Appropriate some code
    
    * Removed unnecessary
    
    Refactoring
    
    * Redoing
    
    * Prodding
    
    * Testing
    
    * Testing
    
    Co-authored-by: ChristofferAAU <cn19@student.aau.dk>
    Co-authored-by: ChristofferAAU <57950913+ChristofferAAU@users.noreply.github.com>

---
+++ GirafRest.IntegrationTest/Setup/WeekTemplateFixture.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GirafRest.IntegrationTest.Setup
+{
+    public class WeekTemplateFixture
+    {
+        public string[] Templates = {$@"{{'thumbnail': {{'id': 28}}, 'name': 'Template1', 'days':
+                         [{{'day': 'Monday', 'activities': [{{'pictograms': [{{'id': 1}}], 'order': 0, 'state': 'Active'}},
+                                                           {{ 'pictograms': [{{ 'id': 6}}], 'order': 0, 'state': 'Active'}}]}},
+                         {{
+    'day': 'Friday', 'activities': [{{ 'pictograms': [{{ 'id': 2}}], 'order': 0, 'state': 'Active'}},
+                                                          {{ 'pictograms': [{{ 'id': 7}}], 'order': 0, 'state': 'Active'}}]}}]}}",
+                         $@"{{
+    'thumbnail': {{ 'id': 29}}, 'name': 'Template2', 'days':
+                         [{{
+        'day': 'Monday', 'activities': [{{ 'pictograms': [{{ 'id': 2}}], 'order': 1, 'state': 'Active'}},
+                                                           {{ 'pictograms': [{{ 'id': 7}}], 'order': 2, 'state': 'Active'}}]}},
+                          {{
+        'day': 'Friday', 'activities': [{{ 'pictograms': [{{ 'id': 3}}], 'order': 1, 'state': 'Active'}},
+                                                           {{ 'pictograms': [{{ 'id': 8}}], 'order': 2, 'state': 'Active'}}]}}]}}"};
+        public string[] templateName = { "Template1", "Template2" };
+        public string GuardianUsername;
+        public string Password;
+        public int TemplateId;
+        public string Citizen1Username;
+        public string Citizen2Username;
+
+        public WeekTemplateFixture()
+        {
+            Citizen1Username = "Citizen-dev";
+            Citizen2Username = $"Alice{DateTime.Now.Ticks}";
+            GuardianUsername = "Guardian-dev";
+            Password = "password";
+            TemplateId = 0;
+        }
+    }
+}
