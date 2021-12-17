8cd757088fc055ae5d12c9b290890154a2c4c8a0
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2021-12-17 11:40:08 +0100

    Updated WeekdayRepository

--- GirafRest/Repositories/WeekdayRepository.cs
+++ GirafRest/Repositories/WeekdayRepository.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using GirafRest.Models;
+﻿using GirafRest.Data;
 using GirafRest.IRepositories;
-using GirafRest.Data;
+using GirafRest.Models;
 using System.Threading.Tasks;
 
 namespace GirafRest.Repositories
@@ -10,8 +8,12 @@ namespace GirafRest.Repositories
     public class WeekdayRepository : Repository<Weekday>, IWeekdayRepository
     {
         public WeekdayRepository(GirafDbContext context) : base(context)
-        {
+        { }
 
+        public async Task<int> DeleteSpecificWeekDay(Weekday oldDay)
+        {
+            Context.Weekdays.Update(oldDay);
+            return await Context.SaveChangesAsync();
         }
         public async Task<int> UpdateSpecificWeekDay(Weekday oldDay)
         {
