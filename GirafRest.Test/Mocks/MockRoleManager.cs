16d3edf5040d9db02ce1108ea2f35bc279d47d7e
Manfred Dahl <74592878+manfredhod@users.noreply.github.com>
2022-10-11 11:01:53 +0200

    Refactor controllers issue/200 & Test DepartmentController 255 (#266) (#269)
    * Refactor controllers issue/200 & Test DepartmentController 255 (#266)
    
    Co-authored-by: djepse20 <djepse20@student.aau.dk>
    Co-authored-by: Thomas Larsen <74708296+TBLarsen@users.noreply.github.com>
    Co-authored-by: ltranu20 <ltranu20@student.aau.dk>

--- GirafRest.Test/Mocks/MockRoleManager.cs
+++ GirafRest.Test/Mocks/MockRoleManager.cs
@@ -1,16 +1,37 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using GirafRest.Models;
+using GirafRest.Models.DTOs;
+using GirafRest.Models.Enums;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-
+using Moq;
 namespace GirafRest.Test.Mocks
 {
     public class MockRoleManager : RoleManager<GirafRole>
     {
-        public MockRoleManager(IRoleStore<GirafRole> store)
-            : base(store, null, null, null, null)
+
+        private readonly List<GirafRoles> rolesList;
+        
+
+        public MockRoleManager(List<GirafRoles> roles)
+           : this(new Mock<IRoleStore<GirafRole>>())
+        {
+            rolesList = roles;
+        }
+        public MockRoleManager(Mock<IRoleStore<GirafRole>> store)
+            : base(store.Object, null, null, null, null)
         {
         }
+
+        public Task<GirafRoles> findUserRole(UserManager<GirafUser> userManager,GirafUser user)
+        {
+            var ele = rolesList[0];
+            this.rolesList.RemoveAt(0);
+            return Task.FromResult<GirafRoles>(ele);
+        }
+
+
     }
 }
