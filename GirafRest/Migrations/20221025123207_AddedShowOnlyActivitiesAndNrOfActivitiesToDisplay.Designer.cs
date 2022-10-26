cfa11a76588dfa9a5646cfc3de4b8eaf10c8ee93
Chris31415926 <75470812+Chris31415926@users.noreply.github.com>
2022-10-26 10:47:31 +0200

    Revert "Revert "Added two new entries to the database""
    This reverts commit 538953d77657f16a74ea0d94c2ca563b8c1ec69b.

--- GirafRest/Migrations/20221025123207_AddedShowOnlyActivitiesAndNrOfActivitiesToDisplay.Designer.cs
+++ GirafRest/Migrations/20221025123207_AddedShowOnlyActivitiesAndNrOfActivitiesToDisplay.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GirafRest.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GirafRest.Migrations
 {
     [DbContext(typeof(GirafDbContext))]
-    partial class GirafDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221025123207_AddedShowOnlyActivitiesAndNrOfActivitiesToDisplay")]
+    partial class AddedShowOnlyActivitiesAndNrOfActivitiesToDisplay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
