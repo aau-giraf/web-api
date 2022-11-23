af09ae3eb6bffde36a58561576da73ddbeb08268
Taoshi <nicolaikh@live.dk>
2022-11-23 13:07:54 +0100

    Settings for weekplanner feature 857 (#303)
    * Added two new entries to the database
    
    Added NrOfActivitiesToDisplay and ShowOnlyActivities to the settings table in the database.
    
    * Revert "Added two new entries to the database"
    
    This reverts commit d814e05a51a1f539830b00c2f5d9291d518a42ce.
    
    * Revert "Revert "Added two new entries to the database""
    
    This reverts commit 538953d77657f16a74ea0d94c2ca563b8c1ec69b.
    
    * Revert "Revert "Revert "Added two new entries to the database"""
    
    This reverts commit cfa11a76588dfa9a5646cfc3de4b8eaf10c8ee93.
    
    * Migrated nrOfActivitiesToDisplay and showOnlyActivities into settings table
    
    * Added new item to settings table in database
    
    Has added a boolean called ShowSettingsForCitizen that is used to indicate whether a citizen shall see their own settings tab.
    
    * Added tests for our new settings
    
    * Fix errors
    
    * Remove breaking change
    
    * Changed class names
    
    Changed AddedShowOnlyActivitiesAndNrOfActivitiesToDisplayToSettingsTable to AddedActivityVariablesToDatabase and  changed AddedShowSettingsForCitizenToSettingsTable to AddedSettingsFlagToDatabase
    
    * Changed a class name + minor changes to former commit
    
    Changed AddedShowPopupColumnToSettingsTable to AddedPopupFlagToDB
    
    * Remove localhost
    
    Co-authored-by: Chris31415926 <75470812+Chris31415926@users.noreply.github.com>
    Co-authored-by: Makel <mikkel@dissing.one>
    Co-authored-by: Chris31415926 <clma17@student.aau.dk>

--- GirafRest.IntegrationTest/Tests/UserControllerTest.cs
+++ GirafRest.IntegrationTest/Tests/UserControllerTest.cs
@@ -382,14 +382,14 @@ namespace GirafRest.IntegrationTest.Tests
             Assert.Equal(60, content["data"]["timerSeconds"]);
             Assert.Equal(3, content["data"]["activitiesCount"]);
             Assert.Equal(3, content["data"]["theme"]);
-            Assert.Equal(2, content["data"]["nrOfDaysToDisplayPortrait"]);
-            Assert.True(content["data"]["displayDaysRelativePortrait"].ToObject<bool>());
-            Assert.Equal(5, content["data"]["nrOfDaysToDisplayLandscape"]);
-            Assert.False(content["data"]["displayDaysRelativeLandscape"].ToObject<bool>());
+            Assert.Equal(2, content["data"]["nrOfDaysToDisplay"]);
+            Assert.Equal(0, content["data"]["nrOfActivitiesToDisplay"]);
             Assert.True(content["data"]["greyScale"].ToObject<bool>());
             Assert.True(content["data"]["lockTimerControl"].ToObject<bool>());
             Assert.True(content["data"]["pictogramText"].ToObject<bool>());
             Assert.False(content["data"]["showPopup"].ToObject<bool>());
+            Assert.False(content["data"]["showOnlyActivities"].ToObject<bool>());
+            Assert.False(content["data"]["showSettingsForCitizen"].ToObject<bool>());
             Assert.Equal("#FF00FF", content["data"]["weekDayColors"][0]["hexColor"]);
             Assert.Equal(1, content["data"]["weekDayColors"][0]["day"]);
         }
