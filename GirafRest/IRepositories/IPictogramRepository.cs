1dbba50a9e8574324bf5b5b434f2a705780cc8c3
Jaffenheimer <74659222+Jaffenheimer@users.noreply.github.com>
2022-11-24 11:38:05 +0100

    Added refactored pictogram controller as well as begun on tests

--- GirafRest/IRepositories/IPictogramRepository.cs
+++ GirafRest/IRepositories/IPictogramRepository.cs
@@ -1,5 +1,6 @@
 using GirafRest.Models;
 using GirafRest.Models.DTOs;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GirafRest.IRepositories
@@ -7,10 +8,12 @@ namespace GirafRest.IRepositories
     /// <summary>
     /// Domain specific repository for pictograms
     /// </summary>
-    public interface IPictogramRepository : IRepository<Models.Pictogram>
-    {        
+    public interface IPictogramRepository : IRepository<Pictogram>
+    {
         public Task<Pictogram> getPictogramMatchingRelation(PictogramRelation pictogramRelation);
+
         public Task<Pictogram> GetPictogramWithName(string name);
+
         /// <summary>
         /// Add empty pictogram with specified name and access level.
         /// saves to database.
@@ -18,14 +21,27 @@ namespace GirafRest.IRepositories
         /// <param name="name"></param>
         /// <param name="access"></param>
         public Task<int> AddPictogramWith_NO_ImageHash(string name, AccessLevel access);
+
+        public Pictogram GetByID(long pictogramID);
+
         Task<Pictogram> FetchResourceWithId(ResourceIdDTO resourceIdDTO);
+
         public Task<Pictogram> FindResource(ResourceIdDTO resourceIdDTO);
+
         public Task<Pictogram> GetPictogramWithID(long Id);
-        /// <summary>
-        /// Fetches the first or default (null) Pictogram by ID
-        /// </summary>
-        /// <param name="pictogramID">The ID of the pictogram to fetch</param>
-        /// <returns>The Pictogram instance or default</returns>
-        Models.Pictogram GetByID(long pictogramID);
+
+        public Task RemoveRelations(Pictogram pict);
+
+        public IEnumerable<Pictogram> fetchPictogramsFromDepartmentStartsWithQuery(string query, GirafUser user);
+
+        public IEnumerable<Pictogram> fetchPictogramsFromDepartmentsContainsQuery(string query, GirafUser user);
+
+        public IEnumerable<Pictogram> fetchPictogramsUserNotPartOfDepartmentStartsWithQuery(string query, GirafUser user);
+
+        public IEnumerable<Pictogram> fetchPictogramsUserNotPartOfDepartmentContainsQuery(string query, GirafUser user);
+
+        public IEnumerable<Pictogram> fetchPictogramsNoUserLoggedInStartsWithQuery(string query);
+
+        public IEnumerable<Pictogram> fetchPictogramsNoUserLoggedInContainsQuery(string query);
     }
 }
