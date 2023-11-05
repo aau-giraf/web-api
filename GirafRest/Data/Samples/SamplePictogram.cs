ecf8ef41b662168f4877b209fc525c8067a84385
Esben Krogh <ekrogh18@student.aau.dk>
2023-11-05 21:25:42 +0100

    Deleted unused methods from Data Folder

--- GirafRest/Data/Samples/SamplePictogram.cs
+++ GirafRest/Data/Samples/SamplePictogram.cs
@@ -5,12 +5,5 @@
         public string Title { get; set; }
         public string AccessLevel { get; set; }
         public string ImageHash { get; set; }
-
-        public SamplePictogram(string name, string accessLevel, string imageHash)
-        {
-            Title = name;
-            AccessLevel = accessLevel;
-            ImageHash = imageHash;
-        }
     }
 }
