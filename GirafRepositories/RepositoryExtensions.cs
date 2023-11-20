5e6286d7e04c2e3364764ba1d0449bb1584a24ef
Kristian <KristianS93@hotmail.com>
2023-11-20 15:08:17 +0100

    added extension methods for IserviceCollection

--- GirafRepositories/RepositoryExtensions.cs
+++ GirafRepositories/RepositoryExtensions.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GirafRest.IRepositories;
+using GirafRest.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GirafRepositories
 {
-    static class RepositoryExtensions
+    public static class RepositoryExtensions
     {
-
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMySql(configuration);
+            // scoped, transient, singleton
+            services.AddTransient<IActivityRepository, ActivityRepository>();
+            return services;
+        }
     }
 }
--- GirafServices/ServiceExtensions.cs
+++ GirafServices/ServiceExtensions.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GirafRest.IRepositories;
+using GirafRest.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GirafServices
 {
-    static class ServiceExtensions
+    public static class ServiceExtensions
     {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+        
+            services.AddSingleton<ISERVICE, MERSERVICE>();
+            return services;
+        }
     }
 }
