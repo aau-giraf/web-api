d7bb3fa81563e5488ec1182a1df844a7d5af6e15
Kristian <KristianS93@hotmail.com>
2023-11-28 14:00:05 +0100

    removed remaining log problems from programoptions

--- GirafAPI/ProgramOptions.cs
+++ GirafAPI/ProgramOptions.cs
@@ -22,19 +22,5 @@ namespace GirafAPI
         /// A field for storing the port on which to host the server.
         /// </summary>
         public static Int16 Port = 5000;
-
-        /// <summary>
-        /// Indicates if the server should utilize file logging.
-        /// </summary>
-        public static bool LogToFile = false;
-        /// <summary>
-        /// The file path of the file to log to.
-        /// </summary>
-        public static string LogFilepath = "";
-
-        /// <summary>
-        /// The directory where log-files should be placed.
-        /// </summary>
-        public static readonly string LogDirectory = Path.Combine("wwwroot", "DebugLogs");
     }
 }
